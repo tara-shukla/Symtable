aaa6ba664b209745cde132f213214b1ee820cc6b
tarashukla <ts6796@princeton.edu>
2023-03-21 19:14:16 -0400

    linkedlist v1

---
+++ symtable.h
@@ -0,0 +1,52 @@
+/*--------------------------------------------------------------------*/
+/* symtable.h                                       */
+/* Author: Tara Shukla                                               */
+/*--------------------------------------------------------------------*/
+#ifndef SYMTABLE_INCLUDED
+#define SYMTABLE_INCLUDED
+#include<stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+struct SymTable;
+typedef struct SymTable *SymTable_T;
+
+/*return a new SymTable object that contains no bindings, 
+or NULL if insufficient memory is available.*/
+SymTable_T SymTable_new(void);
+
+/*free all memory occupied by oSymTable*/
+void SymTable_free(SymTable_T oSymTable);
+
+/*return size_t the number of bindings in oSymTable*/
+size_t SymTable_getLength(SymTable_T oSymTable);
+
+/*add a new binding to oSymTable w key pcKey, value pvValue and return 1 (TRUE)*/
+/*if already exists leave oSymTable unchanged and return 0 (FALSE)*/
+/*if insufficient mem, return 0*/
+int SymTable_put(SymTable_T oSymTable,
+    const char *pcKey, const void *pvValue);
+
+/*if binding w key pcKey exists in oSymTable, replace value w pvValue and return old val*/
+/*else leave unchanged, return null*/
+void *SymTable_replace(SymTable_T oSymTable,
+     const char *pcKey, const void *pvValue);
+
+/*return 1 (TRUE) if oSymTable contains a binding whose key is pcKey, else 0 (FALSE)*/
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+
+/*return the value in oSymTable w key pcKey, or NULL if no such binding exists*/
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
+
+/*remove and return value with key pcKey; if nonexistent, return null*/
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+
+/*apply pfApply to each binding, passing pvExtra as parameter*/
+void SymTable_map(SymTable_T oSymTable,
+     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+     const void *pvExtra);
+
+/*--------------------------------------------------------------------*/
+#endif
---
+++ symtablelist.c
@@ -0,0 +1,186 @@
+/*--------------------------------------------------------------------*/
+/* stra.c                                             */
+/* Author: Tara Shukla                                              */
+/*--------------------------------------------------------------------*/
+
+#include "symtable.h"
+#include <assert.h>
+
+struct Node {
+   /* The binding key */
+    char pcKey;
+    /*the matching value*/
+    void *pvValue;
+
+   /* The address of the next StackNode. */
+   struct Node *next;
+};
+
+struct SymTable{
+    /*address of first node*/
+    struct Node *first;
+    size_t len;
+};
+
+
+SymTable_T SymTable_new(void){
+    SymTable_T oSymTable;
+    oSymTable = (SymTable_T)malloc(sizeof(struct SymTable));
+    if (oSymTable==NULL){
+        return NULL;
+    }
+    oSymTable->first = NULL;
+    oSymTable->len = 0;
+   return oSymTable;
+}
+
+/*helper func: given key, return pointer to node if it exists, NULL otherwise*/
+struct Node * exists(SymTable_T oSymTable,const char *pcKey){
+    struct Node *current;
+    struct Node *next;
+    
+    assert(oSymTable != NULL);
+
+    for (current = oSymTable->first;
+        current != NULL;
+        current = next)
+    {
+        if (current->pcKey == *pcKey){
+            return current;
+        }
+        next = current->next;
+    }
+    return NULL;
+}
+
+void SymTable_free(SymTable_T oSymTable){
+    struct Node *current;
+    struct Node *next;
+    
+    assert(oSymTable != NULL);
+
+   for (current = oSymTable->first;
+        current != NULL;
+        current = next)
+   {
+      next = current->next;
+      free(current);
+   }
+
+   free(oSymTable);
+}
+
+size_t SymTable_getLength(SymTable_T oSymTable){
+    return oSymTable->len;
+}
+
+
+int SymTable_put(SymTable_T oSymTable,
+    const char *pcKey, const void *pvValue){
+    struct Node *newNode;
+    int present;
+    char *pcKeyCopy;
+    assert(oSymTable != NULL);
+
+    present = contains(oSymTable, pcKey);
+    if (present) return 0;
+    else {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        if (newNode == NULL)
+            return 0;
+
+        char* pcKeyCopy = malloc(sizeof(char)* (strlen(pcKey)+1));
+        if (pcKeyCopy==NULL) return 0;
+        strcpy(pcKeyCopy,pcKey);
+        newNode->pcKey = pcKeyCopy;
+        newNode->pvValue = pvValue;
+        newNode->next = oSymTable->first;
+        oSymTable->first = newNode;
+        oSymTable->len ++;
+        return 1;
+    }
+}
+
+
+void *SymTable_replace(SymTable_T oSymTable,
+    const char *pcKey, const void *pvValue){
+    
+    void * oldVal;
+    struct Node *present; 
+    assert (oSymTable!=NULL);
+
+    present = exists(oSymTable, pcKey);
+    if (present == NULL) return NULL;
+
+    oldVal = present->pvValue;
+    present->pvValue = pvValue;
+    return oldVal;
+}
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey){
+    struct Node *present; 
+    assert(oSymTable!=NULL);
+
+    present = exists(oSymTable, pcKey);
+    if (present==NULL) return 0;
+    return 1;
+}
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey){
+    struct Node *present; 
+    assert(oSymTable!=NULL);
+
+    present = exists(oSymTable, pcKey);
+    if (present==NULL) return NULL;
+    return present->pvValue;
+}
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
+    struct Node *current;
+    struct Node *next;
+    struct Node *prev;
+    void *val;
+
+    
+    assert(oSymTable != NULL);
+
+    for (current = oSymTable->first;
+        current != NULL;
+        current = next)
+    {
+        if (current->pcKey == *pcKey){
+            val = current->pvValue;
+            free(current->pcKey);
+            prev->next = next;
+            oSymTable->len--;
+            return val;
+        }
+        next = current->next;
+        prev = current;
+    }
+    return NULL;
+}
+
+void SymTable_map(SymTable_T oSymTable,
+     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+     const void *pvExtra)
+{
+
+    struct Node *current;
+    struct Node *next;
+    
+    assert(oSymTable != NULL);
+    assert(pfApply!= NULL);
+
+    for (current = oSymTable->first;
+        current != NULL;
+        current = next)
+    {
+        
+        /*call (*pfApply)(pcKey, pvValue, pvExtra) for each pcKey/pvValue binding in oSymTable.*/
+
+        (*pfApply)((void*)current->pcKey, (void*)current->pvValue, pvExtra);
+    }
+}
+
+/*--------------------------------------------------------------------*/
