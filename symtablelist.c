78cfa1feaa06b669558f9705c65f284bd5c1cdec
tarashukla <ts6796@princeton.edu>
2023-03-21 20:21:56 -0400

    bugfinding

--- symtablelist.c
+++ symtablelist.c
@@ -150,13 +150,6 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
         current != NULL;
         current = next)
     {
-        if (strcmp((current->pcKey), pcKey)==0){
-            val = current->pvValue;
-            prev->next = next;
-            oSymTable->len--;
-            free(current->pcKey);
-            return (void*)val;
-        }
         next = current->next;
         prev = current;
     }
