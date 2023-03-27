8b8c35508035624d30d85b786688bf39510faf52
tarashukla <ts6796@princeton.edu>
2023-03-27 19:21:12 -0400

    ok ruied it revet

--- symtablehash.c
+++ symtablehash.c
@@ -189,6 +189,14 @@ int SymTable_put(SymTable_T oSymTable,
         newNode = (struct Node*)malloc(sizeof(struct Node));
         if (newNode == NULL) return 0;
 
+   
+        /*check if binding count exceeds bucket count, and if so adjust bucket count*/
+        if (oSymTable->len == (oSymTable->bucketCount)-1){
+            SymTable_expandHash(oSymTable);
+            /*rehash this new node*/
+            hashVal = SymTable_hash(pcKey,oSymTable->bucketCount);
+        }
+
         pcKeyCopy = (char*)malloc(sizeof(char)* (strlen(pcKey)+1));
         if (pcKeyCopy==NULL) {
             free(newNode); 
@@ -204,13 +212,7 @@ int SymTable_put(SymTable_T oSymTable,
         /*set newnode as first val in the list of the hashval*/
         oSymTable->hashVals[hashVal] = newNode;
 
-         oSymTable->len ++;
-
-
-        /*check if binding count exceeds bucket count, and if so adjust bucket count*/
-        if (oSymTable->len == (oSymTable->bucketCount)){
-            SymTable_expandHash(oSymTable);
-        }
+        oSymTable->len ++;
         return 1;
     }
 }
